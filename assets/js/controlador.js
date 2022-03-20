38987d071c7fd57450fce1cc693dff30224f3595
feregc <gfernandoentique37@gmail.com>
2022-03-19 18:58:16 -0600

    creacion formulario registro

---
+++ assets/js/controlador.js
@@ -0,0 +1,11 @@
+// function abrirFormularioRegistro(){
+//     document.getElementById('pantalla-inicio').style.display('none');
+//     // document.getElementById('pantalla-inicio').classList.toggle('visible');
+//     // document.getElementById('header').classList.toggle('visible');
+// }
+// abrirFormularioRegistro();
+// function abrirFormularioLogIn(){
+//     document.getElementById('pantalla-inicio').classList.toggle('visible');
+//     document.getElementById('header').classList.toggle('visible');
+// }
+// abrirFormularioLogIn();
