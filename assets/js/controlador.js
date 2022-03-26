d1016a4fc476f21b215ffe78dd77a916437837c3
feregc <gfernandoentique37@gmail.com>
2022-03-25 23:35:34 -0600

    creacion de pantalla home y creación del JSON categorias

--- assets/js/controlador.js
+++ assets/js/controlador.js
@@ -1,3 +1,50 @@
+//JSON de categorias
+var categorias = {
+    nombre:'Comida Rápida',
+    empresas: {
+        nombre:'empresa 1',
+        banner:'',
+        logo:'',
+        descripcion:'',
+        productos:{
+            nombre:'',
+            imagen:'',
+            descripcion:'',
+            precio:'',
+            cantidad:'',
+        }
+    },
+    nombre:'Cafés y Panaderías',
+    empresas: {
+        nombre:'empresa 1',
+        banner:'',
+        logo:'',
+        descripcion:'',
+        productos:{
+            nombre:'',
+            imagen:'',
+            descripcion:'',
+            precio:'',
+            cantidad:'',
+        }
+    },
+    nombre:'Comida Gourmet',
+    empresas: {
+        nombre:'empresa 1',
+        banner:'',
+        logo:'',
+        descripcion:'',
+        productos:{
+            nombre:'',
+            imagen:'',
+            descripcion:'',
+            precio:'',
+            cantidad:'',
+        }
+    }
+}
+
+//función para mostrar secciones
 function mostrarSeccion(seccion){
     document.getElementById('inicio').style.display = "block";
 
