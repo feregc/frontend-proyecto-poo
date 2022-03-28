823b8b4f531a78bb1f0fb66536d7c90c8b2ec661
feregc <gfernandoentique37@gmail.com>
2022-03-28 02:16:05 -0600

    creacion pantalla de cada empresa y muestra productos

--- assets/js/controlador.js
+++ assets/js/controlador.js
@@ -2,12 +2,131 @@
 var categoria = [
     {
         nombre: 'Comida Rápida',
+        icono: 'fa-burger',
         comercio: [
             {
                 nombreComercio: 'Burger King HN',
-                banner: '',
-                logo: 'assets/img/burgerking.jpg',
+                banner: 'assets/img/banners/cr/bkb.jpg',
+                logo: 'assets/img/logos/cr/burgerking.jpg',
                 descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: 'wopper',
+                        imagen: '',
+                        descripcionProducto: 'aguacate',
+                        precio: 'L 219.00',
+                        cantidad: ''
+                    },
+                    {
+                        nombreProducto: 'king',
+                        imagen: '',
+                        descripcionProducto: 'pollo',
+                        precio: 'L 169.00',
+                        cantidad: '' 
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Chillis',
+                banner: '',
+                logo: 'assets/img/logos/cr/chillis.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Dominos',
+                banner: '',
+                logo: 'assets/img/logos/cr/dominos.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Kentucky Fried Chicken',
+                banner: '',
+                logo: 'assets/img/logos/cr/kfc.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Little Caesars',
+                banner: '',
+                logo: 'assets/img/logos/cr/litllecaesars.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Pizza Hut',
+                banner: '',
+                logo: 'assets/img/logos/cr/pizzahut.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Popeyes',
+                banner: '',
+                logo: 'assets/img/logos/cr/popeyes.jpg',
+                descripcion: 'HAMBURGUESAS - PAPAS - NUGGETS - DESAYUNOS',
+                horaAtencion:'8:00 AM',
+                calificacion:'5.0',
+                tiempoReparto:'30 min',
                 productos: [
                     {
                         nombreProducto: '',
@@ -22,11 +141,102 @@ var categoria = [
     },
     {
         nombre: 'Cafés y Panaderías',
+        icono: 'fa-mug-hot',
         comercio: [
             {
                 nombreComercio: 'Cinnabon',
                 banner: '',
-                logo: 'assets/img/cinnabon.jpg',
+                logo: 'assets/img/logos/cp/cinnabon.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Basilios',
+                banner: '',
+                logo: 'assets/img/logos/cp/basilios.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Churrin Churron',
+                banner: '',
+                logo: 'assets/img/logos/cp/churrin.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Dunkin Donuts',
+                banner: '',
+                logo: 'assets/img/logos/cp/donkin.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Espresso Americano',
+                banner: '',
+                logo: 'assets/img/logos/cp/espresso.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Pan y Más',
+                banner: '',
+                logo: 'assets/img/logos/cp/panymas.jpg',
+                descripcion: 'CINNAMON ROLLS - CAFÉ',
+                productos: [
+                    {
+                        nombreProducto: '',
+                        imagen: '',
+                        descripcionProducto: '',
+                        precio: '',
+                        cantidad: ''
+                    }
+                ]
+            },
+            {
+                nombreComercio: 'Tutis',
+                banner: '',
+                logo: 'assets/img/logos/cp/tutis.jpg',
                 descripcion: 'CINNAMON ROLLS - CAFÉ',
                 productos: [
                     {
@@ -42,11 +252,12 @@ var categoria = [
     },
     {
         nombre: 'Comida Gourmet',
+        icono: 'fa-utensils',
         comercio: [
             {
                 nombreComercio: 'Ni-Fu Ni-Fa',
                 banner: '',
-                logo: 'assets/img/nifunifa.jpg',
+                logo: 'assets/img/logos/cg/nifunifa.jpg',
                 descripcion: 'CARNE - PARRILLADA - ARAGENTINA - CHORIZO',
                 productos: [
                     {
@@ -68,19 +279,57 @@ function esconder() {
     document.getElementById('formulario1-1').classList.add("visible");
     document.getElementById('formulario2').classList.add("visible");
     document.getElementById('home').classList.add("visible");
-    document.getElementById('comidasRapidas').classList.add("visible")
+    document.getElementById('comidasRapidas').classList.add("visible");
+    document.getElementById('menuEmpresas').classList.add("visible");
 }
 
 //muestra la pantalla de registro
 function mostrarRegistro() {
     document.getElementById('inicio').classList.add("visible");
     document.getElementById('formulario1').classList.toggle("visible");
+    document.getElementById('formulario1').innerHTML =
+        `
+    <div id="encabezado">
+            <i id="atras" class="fa-solid fa-arrow-left fa-lg" onclick="atrasLP()"></i>
+            <p class="parrafos">INGRESA TU TELÉFONO</p>
+        </div>
+        <div id="mensaje-registro">
+            <p class="parrafos" style="color: #F12711; margin-bottom: 0.438rem;">Para iniciar, ingresa tu número de
+                teléfono</p>
+            <p class="parrafos" style="color: #F17264;">Te enviaremos un código de verificación</p>
+        </div>
+        <div id="formulario-registro">
+            <input class="caja-texto" id="numero-cel" type="text" placeholder="Ingresa tu número">
+            <div id="check">
+                <input id="" type="checkbox" id="checkRegistro">
+                <label for="checkRegistro"> He leído y acepto los terminos y condiciones</label>
+            </div>
+            <input class="boton boton-Rojo" id="recibeCodigo" type="button" value="RECIBIR CÓDIGO"
+                onclick="mostrarFormReg()">
+        </div>
+    `;
 }
 
 //muestra el formulario de registro
 function mostrarFormReg() {
     document.getElementById('formulario1').classList.toggle("visible");
     document.getElementById('formulario1-1').classList.toggle("visible");
+    document.getElementById('formulario1-1').innerHTML =
+        `
+    <div id="encabezado">
+            <i id="atras" class="fa-solid fa-arrow-left fa-lg" onclick="atrasLP2()"></i>
+            <p class="parrafos">INGRESA TUS DATOS</p>
+        </div>
+        <div id="mensaje-registro">
+            <p class="parrafos" style="color: #F12711; margin-bottom: 0.438rem;">¡Estamos a punto de terminar!</p>
+        </div>
+        <div id="formulario-registro-2">
+            <input class="caja-texto caja" id="nombreCliente" type="text" placeholder="Nombre">
+            <input class="caja-texto caja" id="apellidoCliente" type="text" placeholder="Apellido">
+            <input class="caja-texto caja" id="correoCliente" type="email" placeholder="Correo electrónico">
+            <input class="boton boton-Rojo" id="finalizar" type="button" value="TERMINAR" onclick="mostrarHome()">
+        </div>
+    `;
 }
 
 //mostrar home
@@ -93,6 +342,22 @@ function mostrarHome() {
 function mostrarLogIn() {
     document.getElementById('inicio').classList.add("visible");
     document.getElementById('formulario2').classList.toggle("visible");
+    document.getElementById('formulario2').innerHTML =
+        `
+    <div id="encabezado">
+            <i id="atras" class="fa-solid fa-arrow-left fa-lg" onclick="atrasLP3()"></i>
+            <p class="parrafos">INICIAR SESIÓN</p>
+        </div>
+        <div id="mensaje-registro">
+            <p class="parrafos" style="color: #F12711; margin-bottom: 0.438rem;">Ingresa el número de
+                teléfono</p>
+            <p class="parrafos" style="color: #F17264;">asociado a la cuenta</p>
+        </div>
+        <div id="formulario-registro">
+            <input class="caja-texto caja" id="numero-cel" type="text" placeholder="Ingresa tu número">
+            <input class="boton boton-Rojo" id="recibeCodigo" type="button" value="RECIBIR CÓDIGO" onclick="mostrarHome2()">
+        </div>
+    `;
 }
 
 //mostrar home
@@ -119,32 +384,105 @@ function atrasLP3() {
 }
 
 //función para mostrar secciones
-function mostrarSeccion(seccion){
+function mostrarSeccion(seccion) {
     document.getElementById('inicio').style.display = "block";
 
     document.getElementById(seccion).classList.toggle('visible');
     document.getElementById('inicio').style.display = "none";
 }
 
-
+//Generar automaticamente las categorias
+for (let i = 0; i < categoria.length; i++) {
+    console.log(categoria);
+    document.getElementById('categorias').innerHTML +=
+        `
+    <div class="categorias brand" onclick="mostrarComidas(${i})">
+        <i class="fa-solid ${categoria[i].icono} fa-3x"></i>
+        ${categoria[i].nombre}
+    </div>
+    `;
+}
 
 //función para mostrar comidas Rapidas
-function mostrarComidas() {
-    document.getElementById('comidasRapidas').innerHTML = '';
+function mostrarComidas(index) {
+
     document.getElementById('home').style.display = "none";
-    document.getElementById('comidasRapidas').classList.toggle("visible")
-    let cr = categoria[0];
-    console.log(cr)
-    document.getElementById('comidasRapidas').innerHTML +=
+    document.getElementById('comidasRapidas').style.display = "block"
+    let categoriaSeleccionada = categoria[index];
+    var restaurantesCategoria = categoria[index].comercio;
+    console.log(restaurantesCategoria)
+    document.getElementById('categoriasheader').innerHTML +=
         `
         <i id="atras" class="fa-solid fa-arrow-left fa-lg" style="color: #F12711;"></i>
         <div id="encabezadoEmpresas">
-            <p class="parrafos">${cr.nombre}</p>
+            <p class="parrafos">${categoriaSeleccionada.nombre}</p>
             <input class="caja caja-texto" type="search" name="" id="" placeholder="Buscar">
         </div>
+    `;
+    document.getElementById('comidas').innerHTML = '';
+    for (let i = 0; i < restaurantesCategoria.length; i++) {
+        document.getElementById('comidas').innerHTML +=
+            `
+        <div class="col-6">
+            <div class="card" onclick="mostrarProductos(${i})">
+                <img src=${restaurantesCategoria[i].logo} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-title parrafos">${restaurantesCategoria[i].nombreComercio}<p>
+                </div>
+            </div>
+        </div>
         `;
-    categoria.forEach(function (cat){
-        console.log(cat.nombre);
-    });
+    }
+}
+
+function mostrarProductos(index) {
+    document.getElementById('comidasRapidas').style.display = "none"
+    let restaurantesCategoria = categoria[index].comercio;
+    let pro = restaurantesCategoria[index].productos;
+    console.log(pro);
+    document.getElementById('productos').innerHTML +=
+    `
+    <div id="productosHeader">
+            <img class="rounded-circle position-fixed" src=${restaurantesCategoria[index].logo} alt="">
+            <img class="imgbanner" src=${restaurantesCategoria[index].banner} alt="">
+            <p class="parrafos tituloEmpresa">${restaurantesCategoria[index].nombreComercio}</p>
+            <p class="parrafos descripcionEmpresa">${restaurantesCategoria[index].descripcion}</p>
+            <div class="d-flex flex-row ms-4 infoEmpresas">
+                <i class="fa-regular fa-clock me-2"></i>
+                <p class="me-2 mb-0">${restaurantesCategoria[index].horaAtencion}</p>
+                <i class="fa-regular fa-star me-2"></i>
+                <p class="me-2 mb-0">${restaurantesCategoria[index].calificacion}</p>
+                <i class="fa-solid fa-motorcycle me-2"></i>
+                <p class="me-2 mb-0">${restaurantesCategoria[index].tiempoReparto}</p>
+            </div>
+        </div>
+    `;
+
+    for (let i = 0; i < pro.length; i++) {
+        document.getElementById('menuEmpresas').innerHTML += 
+        `
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                              <div class="col-md-4">
+                                <img src=${pro[i].imagen} class="img-fluid rounded-start" alt="...">
+                              </div>
+                              <div class="col-md-8">
+                                <div class="card-body">
+                                  <h5 class="card-title">${pro[i].nombreProducto}</h5>
+                                  <p class="card-text">${pro[i].descripcionProducto}</p>
+                                  <p class="card-text"><small class="text-muted">${pro[i].precio}</small></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        
+    }
 
 }
