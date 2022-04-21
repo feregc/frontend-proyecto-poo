f54ef303973b562bd6d5c6100c14614f0e08e59f
feregc <gfernandoentique37@gmail.com>
2022-04-20 23:19:51 -0600

    se agregó pantanllas de administar empresas y los productos de la empresa

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -11,5 +11,5 @@ var categoria = [
 ]
 //Generar automaticamente las categorias
 for (let i = 0; i < categoria.length; i++) {
-    console.log(categoria[i]);
+    console.log(categoria[i].nombreCategoria);
 }
